4f79cc39fa7870d88d87eb05f01eb677aa9ce432
Mcnagi <huanjie@protonmail.com>
2024-08-04 21:52:44 +1000

    Update to run trained model Only run the model without training.

--- src/MainActivity.py
+++ src/MainActivity.py
@@ -4,31 +4,32 @@ from stable_baselines3 import SAC
 from stable_baselines3.ppo.policies import MlpPolicy
 from src.Novelty.NoveltyName import NoveltyName
 from src.exceptions.NoModelException import NoModelException
+from src.training.ModelAccess import loadModel
 from src.training.ModelTraining import trainNewModel, continueTrainingModel
 
 # Set up environment for lunar lander
-env = gym.make("LunarLander-v2", render_mode="human", continuous=True)
-# model = SAC("MlpPolicy", env, verbose=1)
-# model.learn(total_timesteps=int(2e5), progress_bar=True)
-# env = model.get_env()
-# observation = env.reset()
+# env = gym.make("LunarLander-v2", render_mode="human", continuous=True)
 
-for _ in range(10):
-    try:
-        model = continueTrainingModel(NoveltyName.ORIGINAL)
-    except NoModelException:
-        model = trainNewModel(env, NoveltyName.ORIGINAL)
+# Training activity
+# for _ in range(10):
+#     try:
+#         model = continueTrainingModel(NoveltyName.ORIGINAL)
+#     except NoModelException:
+#         model = trainNewModel(env, NoveltyName.ORIGINAL)
+#
+#     env = model.get_env()
+#     observation = env.reset()
 
-    env = model.get_env()
-    observation = env.reset()
+# Load latest trained model
+model = loadModel(NoveltyName.ORIGINAL)
+env = model.get_env()
+observation = env.reset()
 
-
-for _ in range(10):
+for _ in range(10000):
     action, _ = model.predict(observation, deterministic=True)
     observation, reward, done, info = env.step(action)
 
     if done:
-        model = continueTrainingModel(NoveltyName.ORIGINAL)
         env = model.get_env()
         observation = env.reset()
 
