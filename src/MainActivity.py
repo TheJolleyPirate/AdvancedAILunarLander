09398245c62bcff0669c7914704bb99175358881
Mcnagi <huanjie@protonmail.com>
2024-08-07 11:35:52 +1000

    Add evaluation in main activity

--- src/MainActivity.py
+++ src/MainActivity.py
@@ -1,20 +1,29 @@
+from stable_baselines3.common.evaluation import evaluate_policy
 from src.Novelty.NoveltyName import NoveltyName
 from src.training.ModelAccess import loadModel
+import numpy as np
+import statistics
 
-# Set up environment for lunar lander
-# env = gym.make("LunarLander-v2", render_mode="human", continuous=True)
 
-# Load latest trained model
-model = loadModel(NoveltyName.ORIGINAL)
-env = model.get_env()
-observation = env.reset()
+def main():
+    n_eval_episodes = 100
 
-for _ in range(10000):
-    action, _ = model.predict(observation, deterministic=True)
-    observation, reward, done, info = env.step(action)
+    # Load latest trained model
+    model = loadModel(NoveltyName.ORIGINAL)
+    env = model.get_env()
 
-    if done:
-        env = model.get_env()
+    mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=n_eval_episodes)
+    print(f"Mean reward for {n_eval_episodes}: {mean_reward}")
+    print(f"Standard deviation of rewards: {std_reward}")
+
+    for _ in range(n_eval_episodes):
         observation = env.reset()
+        done = False
+        while not done:
+            action, _ = model.predict(observation, deterministic=True)
+            observation, reward, done, info = env.step(action)
+    env.close()
 
-env.close()
+
+if __name__ == '__main__':
+    main()
