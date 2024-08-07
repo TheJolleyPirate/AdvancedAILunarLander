12430115327074660d9c6254c23f0ca287d4adbd
Mcnagi <huanjie@protonmail.com>
2024-08-07 11:37:34 +1000

    Implement extensive training

--- src/TrainingActivity.py
+++ src/TrainingActivity.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+from time import sleep
 import gymnasium as gym
 from src.Novelty.NoveltyName import NoveltyName
 from src.exceptions.NoModelException import NoModelException
@@ -6,9 +8,12 @@ from src.training.ModelTraining import continueTrainingModel, trainNewModel
 
 def training_activity(novel_name=NoveltyName.ORIGINAL):
     env = gym.make("LunarLander-v2", render_mode="human", continuous=True)
-    for _ in range(10):
+    start_time = datetime.now()
+    end_time = start_time + timedelta(hours=12)
+    while datetime.now() < end_time:
         try:
             model = continueTrainingModel(NoveltyName.ORIGINAL)
+            sleep(20)
         except NoModelException:
             model = trainNewModel(env, NoveltyName.ORIGINAL)
 
