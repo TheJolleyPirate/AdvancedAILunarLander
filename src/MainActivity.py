61f93bff1cec0ed02e88e47bf249cbb0339a6d71
McNagi <huanjie@protonmail.com>
2024-08-27 07:07:05 +1000

    Update main method

--- src/MainActivity.py
+++ src/MainActivity.py
@@ -1,9 +1,11 @@
+import gymnasium
 from stable_baselines3.common.evaluation import evaluate_policy
 
 from src.Util import adapt_observation
 from src.novelty.NoveltyName import NoveltyName
 from src.novelty.limited_sensor.LunarEnvironment import LunarEnvironment
 from src.training.ModelAccess import loadModel
+import statistics
 
 
 def main():
@@ -12,23 +14,33 @@ def main():
     # Load latest trained model
     model = loadModel(NoveltyName.ORIGINAL)
     shape_trained = model.env.observation_space.shape[0]
-    env = LunarEnvironment()
+    original_env = gymnasium.make("LunarLander-v2", render_mode="human", continuous=True)
+    custom_env = LunarEnvironment()
+    envs = [original_env, custom_env]
+    env_names = ["Original", "Custom-Limited Sensor"]
+    for i in range(len(envs)):
+        print(f"Evaluating environment {env_names[i]}")
+        evaluate(model, envs[i], 100)
 
-    # mean_reward, std_reward = evaluate_policy(model=model, env=env, n_eval_episodes=n_eval_episodes)
-    # print(f"Number of episodes for evaluation: {n_eval_episodes}")
-    # print(f"Mean reward for {n_eval_episodes}: {mean_reward}")
-    # print(f"Standard deviation of rewards: {std_reward}")
 
-    n_presentation_episodes = 10
-    for _ in range(n_presentation_episodes):
+def evaluate(model, env, n_episodes: int = 100):
+    rewards = []
+    shape_trained = model.env.observation_space.shape[0]
+    for _ in range(n_episodes):
+        tmp = 0
         observation, _ = env.reset()
-
         done = False
         while not done:
             observation = adapt_observation(observation, shape_trained)
             action, _ = model.predict(observation, deterministic=True)
             observation, reward, done, truncated, info = env.step(action)
-    env.close()
+            tmp += reward
+        rewards.append(tmp)
+    mean_reward = round(statistics.mean(rewards), 2)
+    std_reward = round(statistics.stdev(rewards), 2)
+    print(f"Number of episodes for evaluation: {n_episodes}")
+    print(f"Mean reward for {n_episodes}: {mean_reward}")
+    print(f"Standard deviation of rewards: {std_reward}")
 
 
 if __name__ == '__main__':
