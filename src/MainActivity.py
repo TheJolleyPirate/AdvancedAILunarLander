5a2e941aa56eb868c1131898f3aa108b3d062ccc
McNagi <huanjie@protonmail.com>
2024-09-05 10:52:35 +1000

    Evaluate model against novelties
    Connect all novelties with novelty director.

--- src/MainActivity.py
+++ src/MainActivity.py
@@ -1,14 +1,6 @@
-import gymnasium
-from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.monitor import Monitor
-
-from environments.atmosphereEnvironment import AtmosphereLunarLander
-from environments.thrusterEnviro import FaultyThrusters
 from src.Util import adapt_observation
-from src.environments.TurbulenceEnv import TurbulenceEnv
-from src.environments.gravityEnvironment import GravityLunarLander
+from src.novelty.NoveltyDirector import NoveltyDirector
 from src.novelty.NoveltyName import NoveltyName
-from src.novelty.limited_sensor.LimitedSensor import LimitedSensor
 from src.training.ModelAccess import loadModel
 import statistics
 
@@ -18,21 +10,13 @@ def main():
 
     # Load latest trained model
     model = loadModel(NoveltyName.ORIGINAL)
-    shape_trained = model.env.observation_space.shape[0]
+    # shape_trained = model.env.observation_space.shape[0]
 
     # load environment
-    original_env = gymnasium.make("LunarLander-v2", render_mode=None, continuous=True)
-    faulty_thruster_env = Monitor(FaultyThrusters(render_mode="human", continuous=True))
-    atmosphere_env = AtmosphereLunarLander(render_mode="human", continuous=True)
-    changing_gravity_env = Monitor(GravityLunarLander(render_mode="rgb_array", continuous=True))
-    turbulence_env = Monitor(TurbulenceEnv(continuous=True))
-    limited_sensor_env = LimitedSensor(render_mode="human")
-
-    envs = [original_env, faulty_thruster_env, atmosphere_env, changing_gravity_env, turbulence_env, limited_sensor_env]
-    env_names = ["Original", "Custom-Limited Sensor"]
-    for i in range(len(envs)):
-        print(f"Evaluating environment {env_names[i]}")
-        evaluate(model, envs[i], 100)
+    for novelty in NoveltyName:
+        env = NoveltyDirector(novelty).build_env(render_mode=None, continuous=True)
+        print(f"Evaluating environment {novelty.name}")
+        evaluate(model, env, n_eval_episodes)
 
 
 def evaluate(model, env, n_episodes: int = 100):
