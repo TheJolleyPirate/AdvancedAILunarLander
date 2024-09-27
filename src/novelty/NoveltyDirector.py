2de0ab88fc8122b95607a376fcaf00b648972a74
Mcnagi <huanjie@protonmail.com>
2024-09-27 12:30:44 +1000

    Wrap env with Montior

--- src/novelty/NoveltyDirector.py
+++ src/novelty/NoveltyDirector.py
@@ -1,7 +1,8 @@
 from src.novelty.NoveltyName import NoveltyName
 from stable_baselines3.common.monitor import Monitor
-import gymnasium as gym
+from stable_baselines3.common.vec_env import DummyVecEnv
 
+import gymnasium as gym
 from src.novelty.environments.TurbulenceEnv import TurbulenceEnv
 from src.novelty.environments.atmosphereEnvironment import AtmosphereLunarLander
 from src.novelty.environments.gravityEnvironment import GravityLunarLander
@@ -14,16 +15,16 @@ class NoveltyDirector:
     def __init__(self, novelty: NoveltyName = NoveltyName.ORIGINAL):
         self.novelty = novelty
 
-    def build_env(self, render_mode=None, continuous: bool = True):
+    def build_env(self, render_mode=None, continuous: bool = True) -> Monitor:
         if self.novelty == NoveltyName.THRUSTER:
             return Monitor(FaultyThrusters(render_mode=render_mode, continuous=continuous))
         if self.novelty == NoveltyName.ATMOSPHERE:
-            return AtmosphereLunarLander(render_mode=render_mode, continuous=continuous)
+            return Monitor(AtmosphereLunarLander(render_mode=render_mode, continuous=continuous))
         if self.novelty == NoveltyName.GRAVITY:
             return Monitor(GravityLunarLander(render_mode=render_mode, continuous=continuous))
         if self.novelty == NoveltyName.TURBULENCE:
             return Monitor(TurbulenceEnv(render_mode=render_mode, continuous=continuous))
         if self.novelty == NoveltyName.SENSOR:
-            return LimitedSensor(render_mode=render_mode, continuous=continuous)
+            return Monitor(LimitedSensor(render_mode=render_mode, continuous=continuous))
         else:
-            return gym.make("LunarLander-v2", render_mode=render_mode, continuous=continuous)
+            return Monitor(gym.make("LunarLander-v2", render_mode=render_mode, continuous=continuous))
--- src/training/ModelTraining.py
+++ src/training/ModelTraining.py
@@ -1,9 +1,13 @@
 import gymnasium as gym
+import matplotlib.pyplot as plt
 from stable_baselines3 import SAC
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import DummyVecEnv
 
 from src.novelty.NoveltyName import NoveltyName
 from src.hyperparameters import LoadHyperparameters
 from src.training.ModelAccess import saveModel, loadModel
+
 
 num_episodes = 500_000
 
@@ -11,6 +15,7 @@ num_episodes = 500_000
 def trainNewModel(env: gym.Env, novelty_name: NoveltyName):
     print("Training new model for novelty: " + novelty_name.value)
     params = LoadHyperparameters.load("../admin/sac.yml")  # FIXME: not good practice of loading file.
+    env = Monitor(DummyVecEnv[lambda: env])
     model = SAC(env=env,
                 batch_size=params.batch_size,
                 buffer_size=params.buffer_size,
@@ -26,6 +31,15 @@ def trainNewModel(env: gym.Env, novelty_name: NoveltyName):
     model.N_TIMESTEPS = params.n_timesteps
 
     model.learn(total_timesteps=model.N_TIMESTEPS, progress_bar=True)
+    
+    # training_results = env.get_episode_rewards()
+    # plt.plot(np.arange(len(training_results)), training_results)
+    # plt.title('Training Process: Episode Rewards')
+    # plt.xlabel('Episode')
+    # plt.ylabel('Reward')
+    # plt.grid()
+    # plt.show()
+
     saveModel(model, novelty_name)
     return model
 
@@ -33,7 +47,7 @@ def trainNewModel(env: gym.Env, novelty_name: NoveltyName):
 def continueTrainingModel(env=None, novelty_name: NoveltyName = NoveltyName.ORIGINAL):
     model = loadModel(novelty_name)
     if env is not None:
-        model.env = env
+        model.env = DummyVecEnv([lambda: env])
     print("Retraining model for novelty: " + novelty_name.value)
     model.learn(total_timesteps=num_episodes)
     saveModel(model, novelty_name)
